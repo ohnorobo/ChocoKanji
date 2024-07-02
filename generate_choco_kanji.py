09ac81bdf1ea98a61c8322c4aebbcea075283cca
ohnorobo <sar.laplante@gmail.com>
2024-07-02 23:43:47 +0200

    vertical flip chars

--- generate_choco_kanji.py
+++ generate_choco_kanji.py
@@ -1,8 +1,7 @@
-# Derived from https://github.com/NicholasARossi/generative_artforms/blob/main/generative_fonts/chinese/generate_chinese_font.py
-
 import pandas as pd
 import xml.etree.ElementTree as ET
 from pprint import pprint
+import svgpathtools
 
 
 header = """<?xml version="1.0" encoding="UTF-8" ?>
@@ -14,49 +13,56 @@ header = """<?xml version="1.0" encoding="UTF-8" ?>
 
 </metadata>
 <defs>
-<font id="ChocoKanji SVG" horiz-adv-x="378" >
+<font id="ChocoKanji SVG" horiz-adv-x="38" >
 <font-face
 font-family="ChocoKanji SVG"
-units-per-em="1000"
-ascent="800"
-descent="-200"
-cap-height="500"
-x-height="300"
+units-per-em="109"
+ascent="80"
+descent="-20"
+cap-height="50"
+x-height="30"
 />
-<missing-glyph horiz-adv-x="800"/>
+<missing-glyph horiz-adv-x="109" d="M 0,0 0,80 80,80 80,0 0,0 M 0,0 80,80 M 20,0 80,60 M 40,0 80,40 M 0,20 60,80"/> 
 """
 footer = """
 </font>
 </defs>
 </svg>"""
 
-x_advance = 1000
+alt_header = """<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+
+<svg xmlns="http://www.w3.org/2000/svg" width="109" height="109" viewBox="0 0 109 109">
+<g style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;">
+"""
+
+alt_footer = """
+</g>
+</svg>"""
+
+
+
+x_advance = 109
 
 KANJI_VG_FOLDER = ""
 
 
-def convert_string_strokes(strokes):
-  running_string = ''
-  for stroke in strokes:
-    current_stroke = ''
-    for i, substroke in enumerate(stroke):
-      if i == 0:
-        current_stroke += f'M {substroke[0]} {substroke[1]} '
-      else:
-        current_stroke += f'L {substroke[0]} {substroke[1]} '
-    running_string += current_stroke
-  return running_string
+def flip_svg_path(d):
+  path = svgpathtools.parse_path(d)
+  pprint(("p", path))
+
+  vertical_flip = svgpathtools.parser.parse_transform('scale(1, -1)')
+  pprint(vertical_flip)
   
-def convert_full_entry(row):
-  return f'<glyph unicode="{row.character}" glyph-name="{row.character}" horiz-adv-x="{x_advance}" d="{row.combined_lines}"/>\n'
+  flipped_path = svgpathtools.path.transform(path, vertical_flip)
+  pprint(flipped_path)
+
+  return flipped_path.d()
 
 
 def fix_path_string(p):
   p = p.rstrip()
-  p = p.replace("M", "M ")
-  p = p.replace("c", " C ")
-  p = p.replace(",", " ")
-
+  p = flip_svg_path(p)
   return p
 
 def convert_kanji(svg: str) -> str:
@@ -65,38 +71,30 @@ def convert_kanji(svg: str) -> str:
   character = root[0][0].attrib['{http://kanjivg.tagaini.net}element']
 
   svg_paths = root.findall(".//*[@d]")
-  pprint(svg_paths)
-
   paths = [fix_path_string(path.attrib['d']) for path in svg_paths]
-  pprint(paths)
-  print(repr(paths[0]))
-
   combined_strokes = " ".join(paths)
-  pprint(combined_strokes)
-  pprint(type(combined_strokes))
 
-  return f'<glyph unicode="{character}" glyph-name="{character}" horiz-adv-x="{x_advance}" d="{combined_strokes}"/>\n'
+  return f'<glyph unicode="{character}" glyph-name="{character}" horiz-adv-x="{x_advance}" d="{combined_strokes}" />\n'
+
+
+def write_kanji_file(open_file, kanji_filename):
+  with open(kanji_filename, 'r') as example_kanji:
+    example_kanji_svg: str = example_kanji.read()
+    entry = convert_kanji(example_kanji_svg)
+
+    pprint(entry)
+
+    open_file.write(entry)
 
 def main():
-  df = pd.read_json('makemeahanzi.json', lines=True)
-  df['combined_lines'] = df.medians.apply(convert_string_strokes)
-  df['full_entries'] = df.apply(lambda x: convert_full_entry(x), axis=1)
-  with open('choco-kanji.svg', 'w') as f:
-    f.write(header)
-    for entry in df['full_entries'].values:
-      f.write(entry)
-    f.write(footer)
-
-def main2():
   with open('choco-kanji.svg', 'w') as f:
     f.write(header)
 
-    with open('04e56.svg', 'r') as example_kanji:
-      example_kanji_svg: str = example_kanji.read()
-      entry = convert_kanji(example_kanji_svg)
-      f.write(entry)
-
+    write_kanji_file(f, '04e56.svg')
+    write_kanji_file(f, '04e57.svg')
+    write_kanji_file(f, '04e58.svg')
+    
     f.write(footer)
 
 
-main2()
+main()
